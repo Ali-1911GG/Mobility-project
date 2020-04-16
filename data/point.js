aa5f8b24979f4c3d9fbc653d0bdd06b35e69fccd
Ali-1911GG <a.abdollahi.c.eng@gmail.com>
2020-04-16 13:09:41 -0700

    my first commit

---
+++ data/point.js
@@ -0,0 +1,12 @@
+data={
+"type": "FeatureCollection",
+"name": "point",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "NAME_0": "Iraq", "CentX": 43.754807, "CentY": 32.999867, "EndX": 54.304033, "EndY": 32.557248, "ID1": 1.0, "ID2": 2.0 }, "geometry": { "type": "Point", "coordinates": [ 43.754807, 43.754807 ] } },
+{ "type": "Feature", "properties": { "NAME_0": "Georgia", "CentX": 43.519272, "CentY": 42.176976, "EndX": 54.304033, "EndY": 32.557248, "ID1": 1.0, "ID2": 3.0 }, "geometry": { "type": "Point", "coordinates": [ 43.519272, 43.519272 ] } },
+{ "type": "Feature", "properties": { "NAME_0": "Iran", "CentX": 54.304033, "CentY": 32.557248, "EndX": 54.304033, "EndY": 32.557248, "ID1": 1.0, "ID2": 1.0 }, "geometry": { "type": "Point", "coordinates": [ 54.304033, 54.304033 ] } },
+{ "type": "Feature", "properties": { "NAME_0": "Estonia", "CentX": 25.549703, "CentY": 58.670634, "EndX": 54.304033, "EndY": 32.557248, "ID1": 1.0, "ID2": 4.0 }, "geometry": { "type": "Point", "coordinates": [ 25.549703, 25.549703 ] } },
+{ "type": "Feature", "properties": { "NAME_0": "Kuwait", "CentX": 47.612991, "CentY": 29.354042, "EndX": 54.304033, "EndY": 32.557248, "ID1": 1.0, "ID2": 5.0 }, "geometry": { "type": "Point", "coordinates": [ 47.612991, 47.612991 ] } }
+]
+}
